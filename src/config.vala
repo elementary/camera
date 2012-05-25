67972a6c42d477080f395c7dde1dfcae5fcdd707
Mario Guerriero <mefrio.g@gmail.com>
2012-05-25 17:00:03 +0200

    fixed bug #1004294

--- src/config.vala
+++ src/config.vala
@@ -6,4 +6,7 @@ public const string RELEASE_NAME = ".";
 public const string VERSION = "0.1";
 public const string VERSION_INFO = "Release";
 public const string PLUGINDIR = "";
+/* Translation template for .desktop file */
+public const string COMMENT = N_("Take photos with your web cam");
+public const string GENERIC = N_("Photo Booth");
 }
--- src/config.vala.cmake
+++ src/config.vala.cmake
@@ -6,4 +6,7 @@ public const string RELEASE_NAME = "@RELEASE_NAME@";
 public const string VERSION = "@VERSION@";
 public const string VERSION_INFO = "@VERSION_INFO@";
 public const string PLUGINDIR = "@PLUGINDIR@";
+/* Translation template for .desktop file */
+public const string COMMENT = N_("Take photos with your web cam");
+public const string GENERIC = N_("Photo Booth");
 }
